0ff179d58ee99649984cb7e743b453e7d5404102
nvantan2 <tannguyen12.07.sk@gmail.com>
2020-07-13 16:38:09 +0700

    feat: features: help-other

--- webpack.config.js
+++ webpack.config.js
@@ -7,96 +7,104 @@ const CopyPlugin = require("copy-webpack-plugin");
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  cache: false,
-  entry: {
-    bundle: "./src/app.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-  },
-  devtool: isDevelopment && "source-map",
-  devServer: {
-    port: 3000,
-    open: false,
-    publicPath: "/",
-    contentBase: path.join(__dirname, "./src"),
-  },
-  module: {
-    rules: [
-      { test: /\.hbs$/, loader: "handlebars-loader" },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: !isDevelopment,
-              minimize: !isDevelopment,
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              autoprefixer: {
-                browsers: ["last 2 versions"],
-              },
-              sourceMap: isDevelopment,
-              plugins: () => [autoprefixer],
-            },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "static/",
-              useRelativePath: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    /** Since Webpack 4 */
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        handlebarsLoader: {},
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name]-styles.css",
-      chunkFilename: "[id].css",
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.join(__dirname, "./src/assets"),
-          to: path.join(__dirname, "./dist/assets"),
-        },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      title: "My awesome service",
-      template: "./src/index.hbs",
-      minify: !isDevelopment && {
-        html5: true,
-        collapseWhitespace: true,
-        caseSensitive: true,
-        removeComments: true,
-        removeEmptyElements: true,
-      },
-    }),
-  ],
+	cache: false,
+	entry: {
+		bundle: "./src/app.js",
+	},
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+	},
+	devtool: isDevelopment && "source-map",
+	devServer: {
+		port: 3000,
+		open: false,
+		publicPath: "/",
+		contentBase: path.join(__dirname, "./src"),
+	},
+	module: {
+		rules: [
+			{ test: /\.hbs$/, loader: "handlebars-loader" },
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: !isDevelopment,
+							minimize: !isDevelopment,
+						},
+					},
+					{
+						loader: "resolve-url-loader",
+						options: {
+							sourceMap: true,
+							// absolute: true,
+						},
+					},
+					{
+						loader: "postcss-loader",
+						options: {
+							autoprefixer: {
+								browsers: ["last 2 versions"],
+							},
+							sourceMap: isDevelopment,
+							plugins: () => [autoprefixer],
+						},
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: isDevelopment,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(jpg|png|gif)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "static/",
+							useRelativePath: true,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		/** Since Webpack 4 */
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				handlebarsLoader: {},
+			},
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name]-styles.css",
+			chunkFilename: "[id].css",
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.join(__dirname, "./src/assets"),
+					to: path.join(__dirname, "./dist/assets"),
+				},
+			],
+		}),
+		new HtmlWebpackPlugin({
+			title: "My awesome service",
+			template: "./src/index.hbs",
+			minify: !isDevelopment && {
+				html5: true,
+				collapseWhitespace: true,
+				caseSensitive: true,
+				removeComments: true,
+				removeEmptyElements: true,
+			},
+		}),
+	],
 };
