baf4a8bd07a49fb1e8a2fc0d63de25cbe5f8f392
trannhai1998 <trannamhai.dn@gmail.com>
2020-07-14 11:06:11 +0700

    Fix review

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -23,8 +23,11 @@ const dataWrapperText = [
   "We extract data from servers and put it in the hands of users, where it’s most valuable.",
   "Simply dummy text of the printing and typesetting industry.",
   "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.",
+  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+  "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary",
+  "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.",
 ];
 setInterval(() => {
   textMain.textContent =
     dataWrapperText[Math.floor(Math.random() * dataWrapperText.length)];
-}, 2500);
+}, 2490);
