5f4fab1c5c649df3fc05b9141d7469deede96991
trannhai1998 <trannamhai.dn@gmail.com>
2020-07-14 11:09:48 +0700

    Fix onload js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,33 +1,35 @@
-const toggle = document.getElementById("toggle_menu");
-const closeBtn = document.getElementById("close_menu");
-const textMain = document.getElementById("textMain");
+window.onload = () => {
+  const toggle = document.getElementById("toggle_menu");
+  const closeBtn = document.getElementById("close_menu");
+  const textMain = document.getElementById("textMain");
 
-function openNav() {
-  document.getElementById("mySidenav").style.right = "0";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
+  function openNav() {
+    document.getElementById("mySidenav").style.right = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.right = "-320px";
-  document.body.style.backgroundColor = "white";
-}
+  function closeNav() {
+    document.getElementById("mySidenav").style.right = "-320px";
+    document.body.style.backgroundColor = "white";
+  }
 
-toggle.addEventListener("click", function () {
-  openNav();
-});
-closeBtn.addEventListener("click", () => {
-  closeNav();
-});
+  toggle.addEventListener("click", function () {
+    openNav();
+  });
+  closeBtn.addEventListener("click", () => {
+    closeNav();
+  });
 
-const dataWrapperText = [
-  "We extract data from servers and put it in the hands of users, where it’s most valuable.",
-  "Simply dummy text of the printing and typesetting industry.",
-  "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.",
-  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
-  "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary",
-  "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.",
-];
-setInterval(() => {
-  textMain.textContent =
-    dataWrapperText[Math.floor(Math.random() * dataWrapperText.length)];
-}, 2490);
+  const dataWrapperText = [
+    "We extract data from servers and put it in the hands of users, where it’s most valuable.",
+    "Simply dummy text of the printing and typesetting industry.",
+    "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.",
+    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+    "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary",
+    "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.",
+  ];
+  setInterval(() => {
+    textMain.textContent =
+      dataWrapperText[Math.floor(Math.random() * dataWrapperText.length)];
+  }, 2490);
+};
